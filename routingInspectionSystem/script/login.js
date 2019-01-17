741f7a9d2fb83f3e7f65a6ca8eb8699d59784e65
Arvin Huang <1095369685@qq.com>
2019-01-17 14:43:19 +0800

    高德地图 偏移获取高德坐标

--- routingInspectionSystem/script/login.js
+++ routingInspectionSystem/script/login.js
@@ -115,13 +115,14 @@ apiready = function() {
         mapObj.plugin('AMap.Geolocation', function() {
             geolocation = new AMap.Geolocation({
                 enableHighAccuracy: true, //是否使用高精度定位，默认:true
-                convert: false, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+                convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
                 showButton: false, //显示定位按钮，默认：true
                 buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
                 showMarker: false, //定位成功后在定位到的位置显示点标记，默认：true
                 showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
                 panToLocation: false, //定位成功后将定位到的位置作为地图中心点，默认：true
-                zoomToAccuracy: false //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+                zoomToAccuracy: false, //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+                useNative: false
             });
             mapObj.addControl(geolocation);
             AMap.event.addListener(geolocation, 'complete', function(data) {
